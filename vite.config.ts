2fa257b6423648fc365cfbe5af8c43c7da288579
Neil Prabhu <neilp@juniper.net>
2024-12-29 14:48:49 -0800

    quick

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-/*
+
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Update this to match your repository name
+  base: '/NeilPrabhu.github.io/', // Update this to match your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
   }
-});
-*/
-
-export default defineConfig({
-  base: '/',
-  plugins: [react()]
 });
