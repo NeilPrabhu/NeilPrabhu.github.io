85a29f0af286c2b594ea2b5a62a587d390b4946e
Neil Prabhu <neilp@juniper.net>
2024-12-31 10:29:50 -0800

    add subdomain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Update this to match your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
